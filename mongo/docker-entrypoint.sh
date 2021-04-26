430ab850f344844c608c9e5b2e0eaf3798fd168b
Grim <icescythe@hotmail.com>
2021-04-26 15:38:03 -0400

    changed some mongo startup

--- mongo/docker-entrypoint.sh
+++ mongo/docker-entrypoint.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-set -eux
-mongod --fork --dbpath /mongo-data/ --syslog --repair
-mongod --shutdown
+set -euv
+
+if [ -f /mongo-data/mongod.lock ]; then
+    echo "Old mongod.lock file found! Removing..."
+    sudo rm /mongo-data/mongod.lock
+    echo "Removed successfully!"
+else
+    echo "No old mongod.lock file found, starting normally..."
+fi
+trap 'kill -2 1; wait 1' SIGTERM
+
 exec "$@"
